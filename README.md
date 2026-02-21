fa90f8a2f95d3c88a799fa4f44751154e0a85d9d
Yash Munde <yashmunde68@gmail.com>
2026-02-21 17:24:03 +0530

    sample frontend folder setup

---
+++ README.md
@@ -0,0 +1,14 @@
+/*
+For frontend : 
+  Zustand
+  react-router-dom
+  tanstack  /react-query
+  react-hook
+  zod
+  recharts
+  
+  
+
+
+
+*/
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Sidebar from "./components/layout/Sidebar";
+
+function App() {
+  return (
+    <>
+      <p>hi </p>
+      <Sidebar/>
+    </>
+  );
+}
+
+export default App;
